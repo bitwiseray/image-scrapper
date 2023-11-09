9ede22c81f07e078262f2832d6f4f00c4ef1a74f
itzryan.777@gmail.com <itzryan.777@gmail.com>
2023-11-09 22:12:22 +0530

    HOPEFULLY THIS FIXES IT

--- src/handlers/checkAssets.js
+++ src/handlers/checkAssets.js
@@ -3,25 +3,13 @@ const fs = require('fs');
 function checkFilesExistence() {
   return new Promise((resolve, reject) => {
     try {
-      const itemsToCheck = {
-        files: ['../afters.json', '../global_url_logs.json'],
-        folders: ['../output']
-      };
-      for (const itemType of Object.keys(itemsToCheck)) {
-        if (itemsToCheck.hasOwnProperty(itemType)) {
-          for (const item of itemsToCheck[itemType]) {
-            if (!fs.existsSync(item)) {
-              if (itemType === 'files') {
-                fs.writeFileSync(item, '[]', 'utf8');
-              } else if (itemType === 'folders') {
-                fs.mkdirSync(item);
-              }
-              console.log('\x1b[33m%s\x1b[0m', `[!] New system files that were originally missing were created.`);
-              resolve({ check: true, error: null });
-            }
-          }
+      const itemsToCheck = ['../afters.json', '../global_url_logs.json'];
+      itemsToCheck.forEach(item => {
+        if (!fs.existsSync(item)) {
+          fs.writeFileSync(item, JSON.stringify([]));
+          console.log(`${item} created.`);
         }
-      }
+      });
       resolve({ check: true, error: null });
     } catch (error) {
       console.log('\x1b[31m%s\x1b[0m', error);
