090b62a881ac708de582905034a448fd93a83af4
itzryan.777@gmail.com <itzryan.777@gmail.com>
2023-11-10 14:51:14 +0530

    Fixed type errors and unexpected behaviours

--- src/handlers/createLogs.js
+++ src/handlers/createLogs.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
+const path = require('path');
 
 async function appendValues(values, page) {
-  fs.readFile('../url_logs.json', 'utf-8', (error, data) => {
+  fs.readFile(path.join(__dirname, '../global_url_logs.json'), 'utf-8', (error, data) => {
     if (error) return console.log('\x1b[31m%s\x1b[0m', error);
     try {
       const jsonData = JSON.parse(data);
       if (!jsonData[page]) jsonData[page] = [];
       jsonData[page] = jsonData[page].concat(values);
-      fs.writeFileSync('url_logs.json', JSON.stringify(jsonData, null, 2), 'utf-8');
+      fs.writeFileSync(path.join(__dirname, '../global_url_logs.json'), JSON.stringify(jsonData, null, 2), 'utf-8');
     } catch (parseError) {
       console.log('\x1b[31m%s\x1b[0m', `[!] Error parsing JSON ${parseError}`);
     }
