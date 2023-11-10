0a3a1e79d11765890f6d469dfb79e6c7d19e6cc5
itzryan.777@gmail.com <itzryan.777@gmail.com>
2023-11-10 15:48:10 +0530

    Imported `afters` file in the configuration

--- src/handlers/userPref.js
+++ src/handlers/userPref.js
@@ -1,5 +1,6 @@
 const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const jsonAfters = require('../afters.json');
 
 const fallback = {
   page: 'ecchi',
--- src/index.js
+++ src/index.js
@@ -22,7 +22,6 @@ checkFilesExistence()
   });
 
 async function sync(fnPage, fnSort, fnLimit, fnFormat) {
-  const jsonAfters = require('./afters.json');
   console.log(...Object.values(userQueue))
   console.log('\x1b[33m%s\x1b[0m', `[!] A logs file will be created of all urls scraped in this session.`);
   let getUrl = jsonAfters[fnPage] ? `https://www.reddit.com/r/${fnPage}/top.json?${fnSort}&after=${jsonAfters[fnPage]}` : `https://www.reddit.com/r/${fnPage}/top.json?${fnSort}`;
